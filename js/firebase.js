653db9df99224f48024c149a07645a21f658ea8d
smanhhya <smanhhya@gmail.com>
2026-05-23 17:57:22 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,6 @@
-// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getDatabase, ref, set, get, push, update, remove, onValue } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
-// إعدادات مشروعك على فايربيز
 const firebaseConfig = {
     apiKey: "AIzaSyCVlbWLaLqYnQVsWgFPTe4PL38bWLTLyuU",
     authDomain: "sman-6512c.firebaseapp.com",
@@ -11,9 +9,8 @@ const firebaseConfig = {
     storageBucket: "sman-6512c.firebasestorage.app"
 };
 
-// تهيئة التطبيق والاتصال
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// تصدير (Export) قاعدة البيانات والدوال عشان باقي الملفات تستخدمها
+// تصدير كل دوال قاعدة البيانات عشان باقي الملفات تشوفها
 export { db, ref, set, get, push, update, remove, onValue };
